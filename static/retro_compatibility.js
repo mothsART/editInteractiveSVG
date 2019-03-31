231afb55633f410d494d2de7bc53258b0f7bcbb5
Ferry Jérémie <ferryjeremie@free.fr>
2019-03-31 13:08:45 +0200

    feature/retrocompatibility

---
+++ static/retro_compatibility.js
@@ -0,0 +1,21 @@
+function version_1_1(el) {
+    "use strict";
+    var descriptions = el.getElementsByClassName('description-content');
+    if (descriptions.length === 0)
+        return;
+    for (var i = 0; i < descriptions.length; i++) {
+        var links = descriptions[i].getElementsByTagName('a');
+        for (var j = 0; j < links.length; j++) {
+            links[j].setAttribute('target', 'blank');
+        }
+    }
+    return el;
+}
+
+function converting(el) {
+    "use strict";
+    var version = document.getElementsByTagName('body')[0].getAttribute('data-version');
+    if (!version)
+        el = version_1_1(el);
+    return el;
+}
