839e51618c9c50234368e30f5d48865976feda8e
Ferry Jérémie <ferryjeremie@free.fr>
2019-04-17 17:00:42 +0200

    bugfix : french corrections

--- static/locale/fr.js
+++ static/locale/fr.js
@@ -159,7 +159,7 @@ const en_messages = {
                                + '<li>This software does not allow you to edit other documents than the SVG format. '
                                + 'It is nevertheless planned to support image formats (png and jpg) in a future version.</li>'
                                + '<li>It is not possible to attach media files in the comments area. '
-                               + '(planned in a future version as well)</li>'                              
+                               + '(planned in a future version as well)</li>'
                                + '</ul>',
     english:                   'english',
     french:                    'french',
@@ -184,7 +184,7 @@ const fr_messages = {
     see_more:                  'En savoir d\'avantage ...',
     load_examples:             'Charger des exemples',
     choose_a_file:             'Sélectionnez un fichier',
-    drag_it_here:              'ou glissez le ici.',
+    drag_it_here:              'ou glissez-le ici.',
     upload:                    'Upload',
     delete:                    'Supprimer',
     add_to_legend:             'Ajouter à la légende',
@@ -205,25 +205,25 @@ const fr_messages = {
     no:                        'Non',
     delete_pic_ans:            'Désirez-vous supprimer cette image ?',
     delete_legend:             'Désirez-vous vraiment supprimer les indices sélectionnés ?',
-    loading_new_pic:           'Chargez une nouvelle image signifie que vous allez perdre votre travail en cours.',
+    loading_new_pic:           'Charger une nouvelle image signifie que vous allez perdre votre travail en cours.',
     are_you_ok:                'Est-ce bien ce que vous souhaitez ?',
     warnings:                  'Avertissements',
     errors:                    'Erreurs',
-    errors_detected:           'Des anomalies majeurs ont été détectées à l\'import.',
+    errors_detected:           'Des anomalies majeures ont été détectées à l\'import.',
     error_file_not_supported:  'Le type de fichier n\est pas supporté',
-    warnings_detected:         'Des anomalies mineurs ont été détectées à l\'import.',
+    warnings_detected:         'Des anomalies mineures ont été détectées à l\'import.',
     warning_style:             'Le fichier importé contient une ou plusieurs balises <strong>&#60;style&#62;</strong>'
-                               +' : toutes les règles css présentent ont été préfixés pour des raisons de sécurité.',
+                               +' : toutes les règles css présentes ont été préfixées pour des raisons de sécurité.',
     warning_script:            'Le fichier importé contient une ou plusieurs balises <strong>&#60;script&#62;</strong> :'
                                + ' tous les scripts javascript ont été supprimés pour des raisons de sécurité.',
     warning_viewbox:           'Le <strong>viewBox calculé</strong> et le <strong>viewBox pré-enregistré</strong>'
-                               + ' dans le document sont radicalement différent.<br />'
+                               + ' dans le document sont radicalement différents.<br />'
                                + 'Si vous constatez qu\'après l\'import, le fichier n\'est pas centré, il est fort probable que le '
-                               + 'soucis provienne de cette différence.',
+                               + 'souci provienne de cette différence.',
     warning_mask:              'Le fichier importé contient une ou plusieurs balises <strong>&#60;mask&#62;</strong> :'
-                               +' la zone visible lors de l\'import peut être radicalement différent du viewBox.',
+                               +' la zone visible lors de l\'import peut être radicalement différente du viewBox.',
     warning_clippath:          'Le fichier importé contient une ou plusieurs balises <strong>&#60;clipPath&#62;</strong> :'
-                               +'  la zone visible lors de l\'import peut être radicalement différent du viewBox.',
+                               +'  la zone visible lors de l\'import peut être radicalement différente du viewBox.',
     github_repo:               'Dépôt Github',
     translators:               'Traducteurs',
     license:                   'Licence',
@@ -245,7 +245,7 @@ const fr_messages = {
     import_description:        '<p>'
                                + 'Pour un nouveau projet, vous devez sélectionner dans un premier temps un fichier SVG statique, '
                                + 'de préférence sans javascript (si le fichier contient du javascript, il sera désactivé).<br />'
-                               + 'Ceci se fait soit via un glisser/déposer du dit document dans la zone en pointillé (<a href="#figure-1">figure 1</a>) '
+                               + 'Ceci se fait soit via un glisser/déposer du dit document dans la zone en pointillés (<a href="#figure-1">figure 1</a>) '
                                + 'soit par le clic sur cette zone ouvrant un explorateur de fichiers (<a href="#figure-2">figure 2</a>).'
                                + '</p>',
     create_legend:             'Création de la légende',
@@ -254,47 +254,47 @@ const fr_messages = {
                                + '</p>'
                                + '<p>Ce mode est scindé en 2 parties :</p>'
                                + '<ul>'
-                               + '<li>un <strong>panneau latérale gauche</strong> (aussi appelé sidebar) contenant le récapitulatif de la légende composé d\'un ou plusieurs indices.</li>'
+                               + '<li>un <strong>panneau latéral gauche</strong> (aussi appelé sidebar) contenant le récapitulatif de la légende composé d\'un ou plusieurs indices.</li>'
                                + '<li>une <strong>zone de prévisualisation</strong> du fichier occupant le reste de l\'espace sur la droite.</li>'
                                + '</ul>'
                                + '<p>'
                                + 'Il est possible d\'ajouter des indices (avec une limite de 99) à votre légende via le bouton vert dédié.<br />'
-                               + 'Par défaut, l\'ajout d\'un indice le place au centre de votre illustration, lui attribut un numéro (incrémentée), '
-                               + 'le rend visible et lui attribut une couleur aléatoire.<br />'
-                               + 'Si vous créer plusieurs indices les un derrières les autres, vous remarquerez que '
-                               + 'l\'algoritme d\'attribution des couleurs n\'est pas complétement aléatoire : il évite les doublons.'
+                               + 'Par défaut, l\'ajout d\'un indice le place au centre de votre illustration, lui attribue un numéro (incrémenté), '
+                               + 'le rend visible et lui attribue une couleur aléatoire.<br />'
+                               + 'Si vous créez plusieurs indices les un derrière les autres, vous remarquerez que '
+                               + 'l\'algoritme d\'attribution des couleurs n\'est pas complètement aléatoire : il évite les doublons.'
                                + '</p>'
                                + '<p>'
                                + 'Une fois cette étape passée, il est possible de déplacer votre indice en le sélectionnant dans '
-                               + 'l\'espace de droite via un clic gauche sans relachement + déplacement et relachement pour figer la position.<br />'
+                               + 'l\'espace de droite via un clic gauche sans relâchement + déplacement et relâchement pour figer la position.<br />'
                                + 'Tant que vous êtes dans le cadre d\'un déplacement, une croix blanche s\'affiche dans le coin supérieur droit de votre indice.'
                                + '</p>'
                                + '<p>'
-                               + 'Chaque indice, peut être masqué via le petit oeil sur sa droite.<br />'
+                               + 'Chaque indice, peut être masqué via le petit œil sur sa droite.<br />'
                                + 'Cette fonctionalité est particulièrement appréciable pour gérer ou éviter les chevauchements entre plusieurs indices.'
                                + '</p>'
                                + '<p>'
-                               + 'En cliquant sur la petite fléche d\'une indice, il est possible de l\'éditer :'
+                               + 'En cliquant sur la petite fléche d\'un indice, il est possible de l\'éditer :'
                                + '</p>'
                                + '<ol>'
                                + '<li>le zoom qui se fait en 2 étapes :'
                                + '<ul>'
                                + '<li>on active le zoom via la case à cocher correspondante</li>'
-                               + '<li>on saisie une valeur</li>'
+                               + '<li>on saisit une valeur</li>'
                                + '</ul>'
                                + 'Dès que vous sortez de la zone de saisie, le zoom est pris en compte sur la zone droite de prévisualisation. '
-                               + 'Il vous es donc facile d\'ajuster ces règlages.'
+                               + 'Il vous est donc facile d\'ajuster ces règlages.'
                                + '</li>'
                                + '<li>Choix d\'une couleur personalisée :<br />'
-                               + 'Comme indiqué précédement, un algorithme choisi à chaque création d\'indice une couleur. '
-                               + 'Il vous est bien évidement possible de choisir manuellement cette couleur via une grille d\'échantions.<br />'
+                               + 'Comme indiqué précédemment, un algorithme choisit à chaque création d\'indice une couleur. '
+                               + 'Il vous est bien évidement possible de choisir manuellement cette couleur via une grille d\'échantillons.<br />'
                                + 'Un premier appui vous donnera par conséquent une gamme de 64 couleurs standards entièrement suffisantes dans la majorité des cas.<br />'
                                + 'Néanmoins, il reste possible d\'affiner encore cette sélection via un second dialogue donnant accès à une pipette de 16 millions de couleurs.'
                                + '</li>'
                                + '<li>'
                                + 'Un bouton vert dans la zone d\'édition d\'un indice vous donne la possibilité d\'attribuer un titre et un descriptif à votre indice.'
-                               + 'L\'appuie en dehors de la zone de dialogue fermera automatiquement le dialogue et enregistrera votre travail.<br />'
-                               + 'Les commentaires peuvent être enrichies par des zones en italiques, en gras, des tailles de texte, des images etc.<br />'
+                               + 'L\'appui en dehors de la zone de dialogue fermera automatiquement le dialogue et enregistrera votre travail.<br />'
+                               + 'Les commentaires peuvent être enrichis par des zones en italique, en gras, des tailles de texte, des images etc.<br />'
                                + 'Su vous désirez des explications avancées sur cet éditeur, le mieux est de consulter le site du plugin correspondant : <br />'
                                + '<a href="https://alex-d.github.io/Trumbowyg" target="_blank">Editeur Trumbowyg</a>'
                                + '</li>'
@@ -302,10 +302,10 @@ const fr_messages = {
                                + 'Vous pouvez réordonner les indices : pour celà, rien de plus simple que d\'intervertir 1 ou plusieurs indices par des glissé/déposé.'
                                + '</li>'
                                + '<li>'
-                               + 'Enfin, il vous est possible de supprimer 1 ou plusieurs indices en les cochants (case à cocher sur la droite)'
+                               + 'Enfin, il vous est possible de supprimer 1 ou plusieurs indices en les cochant (case à cocher sur la droite)'
                                + ' puis d\'appuyer sur le bouton de suppression.<br />'
                                + 'Cette manipulation délicate nécessitera de répondre par l\'affirmative dans une '
-                               + 'fenêtre modale sous peine de ne pas être pris en compte.'
+                               + 'fenêtre modale sous peine de ne pas être prise en compte.'
                                + '</li>'
                                + '</ol>',
     save:                      'Enregistrer son travail',
@@ -320,12 +320,12 @@ const fr_messages = {
                                + 'Le fichier HTML précédement sauvegardé est effectivement rééditable à volonté.'
                                + 'Pour ce faire, il vous suffit de passer par le même procédé qu\'un import '
                                + 'de fichier SVG (glissé/déposé ou recherche du fichier avec l\'explorateur).'
-                               + 'Le logiciel récupèrera l\'ensemble des informations et il vous sera possible '
+                               + 'Le logiciel récupérera l\'ensemble des informations et il vous sera possible '
                                + 'de le modifier et d\'enregistrer par la suite un nouveau document.'
                                + '</p>',
     new_import:                'Nouvel import',
     new_import_description:    '<p>'
-                               + 'Il est a tout moment, possible de supprimer le fichier en cours.<br />'
+                               + 'Il est à tout moment, possible de supprimer le fichier en cours.<br />'
                                + 'Pour celà, il vous suffit de cliquer sur le bouton supprimer dans la '
                                + '<strong>zone de prévisualisation</strong> et de confirmer dans la fenêtre modale qui lui succède.'
                                + '</p>'
@@ -335,14 +335,14 @@ const fr_messages = {
     view_mode:                 'Mode de visualisation',
     view_mode_description:     '<p>'
                                + 'Le mode de visualisation vous permet d\'interagir avec votre illustration exactement comme '
-                               + 'si elle venait d\'être publié à ceci près que vous pouvez repassé en édition et continuer votre travail en cours.<br />'
+                               + 'si elle venait d\'être publiée à ceci près que vous pouvez repasser en édition et continuer votre travail en cours.<br />'
                                + 'Ce mode vous donne donc accès à un contrôle sur votre production définitive.'
                                + '</p>',
     error_import:              'Des soucis d\'import ?',
     error_import_description:  '<p>'
-                               + 'Après l\'import d\'un fichier, une série de contrôles qualités est effectué.<br />'
-                               + 'Si la moindre anomalie est détecté, une zone d\'avertissement sera visible : il vous '
-                               + 'sera possible d\'avoir des informations plus précise sur les anomalies rencontrés si besoin et éventuellement y remédier.'
+                               + 'Après l\'import d\'un fichier, une série de contrôles qualité est effectué.<br />'
+                               + 'Si la moindre anomalie est détectée, une zone d\'avertissement sera visible : il vous '
+                               + 'sera possible d\'avoir des informations plus précises sur les anomalies rencontrées si besoin et éventuellement y remédier.'
                                + '</p>',
     limitation:                'Limitation',
     limitation_description:    '<ul>'
